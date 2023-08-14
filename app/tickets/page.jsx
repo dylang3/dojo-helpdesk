645ba94f34ff1b88dbf1ca0cb6ebfb6de8d22011
Dylan Guthrie <dylankguthrie@gmail.com>
2023-08-13 22:22:33 -0500

    added styles and TicketList component

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,3 +1,17 @@
+import TicketList from './TicketList'
+
 export default function Tickets() {
-	return <h2>Tickets</h2>
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
+    </main>
+  )
 }
