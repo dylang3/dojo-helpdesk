c55cdbe4f0c66afd5f2afae90f73ba04a38f1ea9
Dylan Guthrie <dylankguthrie@gmail.com>
2023-09-06 20:17:48 -0500

    added supabase auth

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,9 +1,9 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 // Components
 import Navbar from '../components/Navbar'
-import { redirect } from 'next/navigation'
 
 export default async function DashboardLayout({ children }) {
 	const supabase = createServerComponentClient({ cookies })
