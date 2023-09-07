0f38c6d5afd99fa5c1fcdb0915af8c3bf780b4cb
Dylan Guthrie <dylankguthrie@gmail.com>
2023-09-06 20:17:15 -0500

    added supabase auth

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from './dojo-logo.png'
+import LogoutButton from './LogoutButton'
 
-export default function Navbar() {
+export default function Navbar({ user }) {
 	return (
 		<nav>
-			<Image src={logo} alt="Dojo Helpdesk logo" width={70} quality={100} placeholder="blur" />
+			<Image
+				src={logo}
+				alt="Dojo Helpdesk logo"
+				width={70}
+				quality={100}
+				placeholder="blur"
+			/>
 			<h1>Dojo Helpdesk</h1>
 			<Link href="/">Dashboard</Link>
-			<Link href="/tickets">Tickets</Link>
+			<Link
+				href="/tickets"
+				className="mr-auto"
+			>
+				Tickets
+			</Link>
+
+			{user && <span>Hello, {user.email}</span>}
+			<LogoutButton />
 		</nav>
 	)
 }
